3b197d8d73c2744eebe436141ca2f921b964f487
marcelooliveira <mclricardo@gmail.com>
2018-05-03 17:20:32 -0300

    Formação .NET Junior

---
+++ CasaDoCodigo/Repositories/CadastroRepository.cs
@@ -0,0 +1,35 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICadastroRepository
+    {
+        Cadastro Update(int cadastroId, Cadastro novoCadastro);
+    }
+
+    public class CadastroRepository : BaseRepository<Cadastro>, ICadastroRepository
+    {
+        public CadastroRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public Cadastro Update(int cadastroId, Cadastro novoCadastro)
+        {
+            var cadastroDB = dbSet.Where(c => c.Id == cadastroId)
+                .SingleOrDefault();
+
+            if (cadastroDB == null)
+            {
+                throw new ArgumentNullException("cadastro");
+            }
+
+            cadastroDB.Update(novoCadastro);
+            contexto.SaveChanges();
+            return cadastroDB;
+        }
+    }
+}
